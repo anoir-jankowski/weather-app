024efac228489e192634d4b6486e9c3240fdb42b
Anoir Jankowski <jankowskianoir0@gmail.com>
2026-04-17 16:44:52 +0200

    add weather api integration with fetch

--- script.js
+++ script.js
@@ -2,19 +2,35 @@ const cityInput = document.getElementById("cityInput");
 const searchBtn = document.getElementById("searchBtn");
 const weatherResult = document.getElementById("weatherResult");
 
+const apiKey = "c422c5dbd472f674d49215745cf062be";
+
 searchBtn.addEventListener("click", function() {
-    const city = cityInput.value;
+    const city = cityInput.value.trim();
 
     //check if its empty
     if(city===""){
         weatherResult.textContent = "Please enter a city";
         return;
     }
-    
-    // fake data for now 
-    const temperatur = 25;
-    const condition = "Sunny";
 
-    weatherResulte.textContent = 
-        "Weather in " + city + ": " + temperatur + "°C, " + condition;
+    // API URL
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="
+                + city + "&units=metric&appid=" + apikey;
+
+    // fetch = gets data from web
+    fetch(url)
+        .then(function(response) {
+            return response.json();  // converts into JS object
+        })
+        .then(function(data) {
+
+            const temperatur = data.main.temp;
+            const condition = data.weather[0].main;
+
+            weatherResult.textContent = 
+                "Weather in " + city + ": " + temperature + "°C " + "condition: " + condition;
+        })
+        .catch(function(error) {
+            weatherResult.textContent = "Error fetching data";
+        });
 });
